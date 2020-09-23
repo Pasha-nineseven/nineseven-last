4d64d83054a74a2147c2a91654d79101bc7c6565
want2stayhere <want2stayhere@gmail.com>
2020-09-23 20:57:14 +0300

    1,3,4,7,8,10  +

--- build/js/interface.js
+++ build/js/interface.js
@@ -31,6 +31,9 @@ $(document).ready(function() {
             slidesToShow: 1,
             slidesToScroll: 1,
             adaptiveHeight: false,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            pauseOnHover:false,
         });
 
         var slideCount = $slider_rec.slick("getSlick").slideCount;
@@ -58,6 +61,38 @@ $(document).ready(function() {
             $('.slick-dots').hide();
         }
     };
+
+
+
+
+    $('body').on('click','.cases-toggle__item', function(e){
+        e.preventDefault();
+        $('.cases-toggle__item').removeClass('active');
+        $(this).addClass('active');
+        var filter = $(this).data('filter');
+        if (filter == 'all') {
+            $('.cases-item').show();
+        } else{
+            $('.cases-item').hide();
+            $('.cases-item[data-category="'+filter+'"]').show();
+        }
+    });
+
+
+
+    if ($('.more__counter').length>0) {
+        $('.more__counter').each(function () {
+            $(this).prop('Counter',0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 1000,
+                // easing: 'linear',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    }
 });
 
 
