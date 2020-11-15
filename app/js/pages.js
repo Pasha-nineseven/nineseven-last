b820c2f7c12f2c6ee929213c6e2cf39100dba0f9
Pasha-nineseven <pavel@nineseven.ru>
2020-11-15 17:25:07 +0300

    doc-fix-part3

--- app/js/pages.js
+++ app/js/pages.js
@@ -30,5 +30,6 @@ $('body').append(
 		<li><a href="news.html">News</a></li> \
 		<li><a href="news-in.html">News-in</a></li> \
 		<li><a href="news-in2.html">News-in2</a></li> \
+		<li><a href="news-in3.html">News-in3</a></li> \
 	</ol> \
 </div>');
